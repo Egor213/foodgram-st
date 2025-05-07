d38dc657aae4c69a8f6af63edc7dbac0ba12518e
Egor213 <zamok7707@gmail.com>
2025-05-07 16:21:44 +0300

    add ingredient enpoints

--- backend/ingredients/apps.py
+++ backend/ingredients/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class IngredientConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ingredients"
+    verbose_name = "Ингредиенты"
