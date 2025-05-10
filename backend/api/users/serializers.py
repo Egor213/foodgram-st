f386afb262cd5ea5be20b0225840f49c4f021110
Egor213 <zamok7707@gmail.com>
2025-05-10 15:39:57 +0300

    add user serializer

--- backend/api/users/serializers.py
+++ backend/api/users/serializers.py
@@ -1,4 +1,8 @@
-from djoser.serializers import UserCreateSerializer, UserSerializer
+from djoser.serializers import (
+    UserCreateSerializer,
+    UserSerializer,
+    serializers,
+)
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -20,6 +24,26 @@ class CustomCreateUserSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = "__all__"
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "is_subscribed",
+            "avatar",
+        )
+        read_only = (
+            "id",
+            "is_subscribed",
+        )
+
+    def get_is_subscribed(self, author):
+        current_user = self.context.get("request").user
+        if current_user.is_anonymous:
+            return False
+        return current_user.followers.filter(author=author).exists()
