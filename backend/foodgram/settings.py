5c9b6d984bdcfaa86a86eca08df2d8b7b4f9a344
Egor213 <zamok7707@gmail.com>
2025-05-11 15:42:11 +0300

    add db in docker compose and create dump db

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, "..", "infra", ".env"))
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
