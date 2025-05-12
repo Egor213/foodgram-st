094c0d6c5d7f90b0ce5add8f5fc3846d86e84488
Egor213 <zamok7707@gmail.com>
2025-05-13 00:23:49 +0300

    change HttpResponceRedirect to redirect

--- backend/short_urls/views.py
+++ backend/short_urls/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.views import APIView
 
 from .models import ShortUrl
@@ -10,6 +9,4 @@ class ShortUrlAPIView(APIView):
     def get(self, request, short_url):
         short_url = f"/s/{short_url}/"
         obj = get_object_or_404(ShortUrl, short_url=short_url)
-        return HttpResponseRedirect(
-            request.build_absolute_uri(f"{obj.origin_url}")
-        )
+        return redirect(request.build_absolute_uri(f"{obj.origin_url}"))
