6e33cc9b8bf873741cf3552670f262930d0f93e4
Egor213 <zamok7707@gmail.com>
2025-05-20 17:38:31 +0300

    fix users admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser, Subscription
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     search_fields = ("username", "email")
     search_help_text = "Поиск по нику или электронной почте"
 
